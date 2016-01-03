7e0c369fb9049151aae18c879347e5edfde73917
Ryan Goldstein <ryanisaacg@gmail.com>
2016-01-03 14:34:27 -0500

    Switched to writing the C code to a file to demonstrate the capabilities of the program

--- src/main.c
+++ src/main.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 
 int main() {
-	node *print = newNode("printf", CALL, 2);
-	node *fmtString = newNode("\"%f\"", TOKEN, 0);
-	node *cast = newNode("float", CAST, 1);
-	node *plus = newNode("+", OPERATOR, 2);
-	node *op1 = newNode("12", TOKEN, 0);
-	node* op2 = newNode("4", TOKEN, 0);
-	cast->children[0] = plus;
-	plus->children[0] = op1;
-	plus->children[1] = op2;
-	print->children[0] = fmtString;
-	print->children[1] = cast;
-	printf("%s", eval(print));
+	FILE *out = fopen("out.c", "w");
+	if(out) {
+		node *assign = newNode("int x", ASSIGN, 1);
+		node *value = newNode("0", TOKEN, 0);
+		assign->children[0] = value;
+		fprintf(out, "int main() { %s; return x; }", eval(assign));
+	} else {
+		printf("File system error.");
+	}
 	return 0;
 }
